64d2ec3cee9ee865065f3b550656fc54af290a71
Cloud 247 <sebastian@cloud247.no>
2026-09-20 18:20:13 +0200

    Add Android TV pairing component: QrCodeRenderer.kt

---
+++ android-tv/app/src/main/java/no/cloud247/tv/QrCodeRenderer.kt
@@ -0,0 +1,21 @@
+package no.cloud247.tv
+
+import android.graphics.Bitmap
+import android.graphics.Color
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.qrcode.QRCodeWriter
+
+object QrCodeRenderer {
+    fun render(content: String, size: Int = 320): Bitmap {
+        val matrix = QRCodeWriter().encode(content, BarcodeFormat.QR_CODE, size, size)
+        val pixels = IntArray(size * size)
+        for (y in 0 until size) {
+            for (x in 0 until size) {
+                pixels[y * size + x] = if (matrix[x, y]) Color.BLACK else Color.WHITE
+            }
+        }
+        return Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888).apply {
+            setPixels(pixels, 0, size, 0, 0, size, size)
+        }
+    }
+}
