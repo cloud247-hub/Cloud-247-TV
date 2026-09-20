4a15f2146045f7db4d6d811aa66d07c7289fbc4c
Cloud 247 <sebastian@cloud247.no>
2026-09-20 20:01:41 +0200

    Bump Android TV to v1.1.2

--- android-tv/app/build.gradle.kts
+++ android-tv/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "no.cloud247.tv"
         minSdk = 23
         targetSdk = 36
-        versionCode = 10101
-        versionName = "1.1.1"
+        versionCode = 10102
+        versionName = "1.1.2"
     }
 
     buildTypes {
